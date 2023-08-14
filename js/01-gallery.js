44e0e9d36999798db5dd115d81c00e611e7bdd15
Ap01lY0n <128406680+Ap01lY0n@users.noreply.github.com>
2023-08-14 21:27:07 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const imageList = document.querySelector('.gallery');
+const createdImagesMarkup = createImagesMarkup(galleryItems);
+
+imageList.insertAdjacentHTML("beforeend", createdImagesMarkup);
+imageList.addEventListener('click', onClickModalImage);
+
+function createImagesMarkup(arr) {
+    return arr.map(
+        ({ preview, original, description }) =>
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+                </a>
+            </li>`
+    ).join('');
+}
+
+function onClickModalImage(event) {
+    event.preventDefault();
+    window.addEventListener('keydown', onEscClick);
+    
+    const imgOriginalEl = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${imgOriginalEl}">`);
+    instance.show();
+}
+
+function onEscClick(event) {
+    if (event.code === 'Escape') {
+        onModalClose();
+    }
+}
+
+function onModalClose() {
+    window.removeEventListener('keydown', onEscClick);
+    basicLightbox.close();
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const imageList = document.querySelector('.gallery');
+const createdImagesMarkup = createImagesMarkup(galleryItems);
+
+imageList.insertAdjacentHTML("beforeend", createdImagesMarkup);
+
+function createImagesMarkup(arr) {
+    return arr.map(
+        ({ preview, original, description }) =>
+            `<li class="gallery_item">
+                <a class="gallery_link" href="${original}">
+                    <img class="gallery_image" src="${preview}" alt="${description}" />
+                </a>
+            </li>`
+    ).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery_link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
